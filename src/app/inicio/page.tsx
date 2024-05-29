6c070067e37485eef30e1979b607be0046138e75
Christian Leyva <53919610+Chrisley304@users.noreply.github.com>
2024-05-28 23:01:53 -0600

    Added responsive to home view

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -8,8 +8,8 @@ import ContentPreviewCard from "@/components/General/ContentPreviewCard";
 export default function Inicio() {
     return (
         <>
-            <header className="flex container mx-auto py-10">
-                <div className="flex w-1/2 flex-col justify-center">
+            <header className="grid md:grid-cols-2 container mx-auto py-10">
+                <div className="order-2 md:order-1 flex w-full flex-col justify-center p-5 md:p-0">
                     <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
                         Stranger Dings
                     </h2>
@@ -39,7 +39,7 @@ export default function Inicio() {
                         </Button>
                     </div>
                 </div>
-                <div className="w-1/2">
+                <div className="order-1 md:order-2 w-full px-5 md:px-0">
                     <Image
                         src={strangerThingsHeader}
                         className="rounded-xl"
@@ -47,12 +47,12 @@ export default function Inicio() {
                     />
                 </div>
             </header>
-            <main className="container mx-auto mt-5">
+            <main className="container mx-auto mt-0 px-5 md:px-0 md:mt-5">
                 <section className="mt-5">
                     <h3 className="text-xl font-extrabold">
                         Nuevos lanzamientos
                     </h3>
-                    <div className="columns-4 mt-5 gap-5">
+                    <div className="grid grid-cols-2 md:grid-cols-4 mt-5 gap-5">
                         <ContentPreviewCard
                             title="Mujer con audifonos"
                             category="Drama"
@@ -106,7 +106,7 @@ export default function Inicio() {
                 </section>
                 <section className="mt-5">
                     <h3 className="text-xl font-extrabold">Historial</h3>
-                    <div className="columns-4 mt-5 gap-5">
+                    <div className="grid grid-cols-2 md:grid-cols-4 mt-5 gap-5">
                         <ContentPreviewCard
                             title="Mujer con audifonos"
                             category="Drama"
